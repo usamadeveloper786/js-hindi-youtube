bc676f8c51151f7842cc40bcaf026af33aea14ed
Muhammad Usama <125027454+usamadeveloper786@users.noreply.github.com>
2024-01-09 19:36:09 +0000

    update variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 44668
+let accountEmail = "usama@gmail.com"
+var accountPassword = "4466"
+accountCity = "Karachi"
+let accountState;
+// accountId = 7789
+accountEmail = "usamadeveloper@gmail.com"
+accountPassword = "7798"
+accountCity = "Lahore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
